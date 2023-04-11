84397a59468c963af1bb2be804a6ddfa5a55acab
Legion\Whatever <Whatever@gmail.com>
2023-04-11 16:28:17 +0300

    Added an image effect file

---
+++ Assets/ImageEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageEffect : MonoBehaviour
+{
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        
+    }
+}
